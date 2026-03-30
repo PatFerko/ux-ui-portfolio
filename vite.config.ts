4d64b27c90edc4d08cec56e8f7142518cfdde654
Alan Ortega <alan.ortega@smartstream-stp.com>
2026-03-30 08:54:51 +0200

    build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteImageOptimizer({
-      png: { quality: 80 },
-      jpeg: { quality: 80 },
-      webp: { quality: 80, lossless: false },
-      avif: { quality: 65, lossless: false },
-    }),
   ],
   build: {
     assetsInlineLimit: 4096,       // inline assets < 4 KB as base64
